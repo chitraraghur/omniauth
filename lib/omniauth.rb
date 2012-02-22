e65e1e62c3f05c4a946e7be10f9b050f0a1b4f48
chitraraghur <chitra@ereperez.com>
2012-02-22 21:48:29 +1100

    Update lib/omniauth.rb

--- lib/omniauth.rb
+++ lib/omniauth.rb
@@ -1,19 +1,6 @@
-if Rails.env == "production"
-02.
-# no production app yet
-03.
-else
-04.
-if ENV['USER'] == 'monica'
-05.
+1
+2
+3
 Rails.application.config.middleware.use OmniAuth::Builder do
-06.
-provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-07.
-provider :facebook, 'APP_ID', 'APP_SECRET'  # Find these values on the Facebook App page
-08.
-end
-09.
-end
-10.
+  provider :facebook,   131891163565986, 80889163b7863c9f2d412cfc34054f05
 end
